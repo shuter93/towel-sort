a7843acd8fe6e0479dfc22f9b6bad2177872e78b
shuter93 <shuter.1993@gmail.com>
2022-07-28 20:45:34 +0300

    feat: add function towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+const { sort } = require("semver");
+
+module.exports = function towelSort (matrix) {  
+  if(!Array.isArray(matrix)) {
+    return [];
+  }  
+  let sort = matrix.reduce((res,item, index) => {                
+    if (index % 2 == 0 ) {
+      res = res.concat(item.sort((a, b) =>  a - b));
+    } else {
+      res = res.concat(item.sort((a, b) =>  b - a));
+    }    
+    return res;                  
+  }, []);
+  return sort;
 }
+
